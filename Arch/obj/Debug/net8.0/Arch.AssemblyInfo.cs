71e33125396ff9b154e7e598735b3e30fac76f09
FatmaMohamed16 <kamelfatmamohamed@gmail.com>
2025-09-06 16:31:27 +0300

    sourse

--- Arch/obj/Debug/net8.0/Arch.AssemblyInfo.cs
+++ Arch/obj/Debug/net8.0/Arch.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Arch")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+98a707516509dfd05b9a7b045fcff99dbf49d402")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6103b60f90cb820c20dea92c2f756eb3f88e70d")]
 [assembly: System.Reflection.AssemblyProductAttribute("Arch")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Arch")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
